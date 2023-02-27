9e6d10168065a3b9003c74994f3efe38da933b00
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-02-27 12:37:22 +0100

    Add entity fields

--- src/main/java/com/example/lab1jakartaee/entity/Customer.java
+++ src/main/java/com/example/lab1jakartaee/entity/Customer.java
@@ -1,4 +1,83 @@
 package com.example.lab1jakartaee.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Name can't be null")
+    @Size(min = 2)
+    String name;
+
+    @NotNull(message = "Surname can't be null")
+    @Size(min = 2)
+    String surname;
+
+    @NotNull(message = "Email can't be null")
+    String email;
+
+    @NotNull
+    @Size(min = 10, max = 10, message = "Phone number should be ten numbers")
+    Integer phoneNumber;
+
+    @NotNull
+    @Size(min = 10, max = 12, message = "Social security number should be ten or twelve numbers")
+    Integer ssn;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(Integer ssn) {
+        this.ssn = ssn;
+    }
 }
