c173ed444572ec313993f2be0fdf5df34735550f
Henrik-Bengtsson <112540515+Henrik-Bengtsson@users.noreply.github.com>
2023-02-27 13:49:49 +0100

    Add get all in controller

--- src/main/java/com/example/lab1jakartaee/controller/CustomerController.java
+++ src/main/java/com/example/lab1jakartaee/controller/CustomerController.java
@@ -1,4 +1,24 @@
 package com.example.lab1jakartaee.controller;
 
+import com.example.lab1jakartaee.entity.Customer;
+import com.example.lab1jakartaee.repository.CustomerRepository;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+
+@Path("/customers")
+@Produces(MediaType.APPLICATION_JSON)
 public class CustomerController {
+
+    @Inject
+    CustomerRepository repository;
+
+    @GET
+    public List<Customer> getAll(){
+        return repository.findAll();
+    }
 }
--- src/main/java/com/example/lab1jakartaee/repository/CustomerRepository.java
+++ src/main/java/com/example/lab1jakartaee/repository/CustomerRepository.java
@@ -1,4 +1,23 @@
 package com.example.lab1jakartaee.repository;
 
+import com.example.lab1jakartaee.entity.Customer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+
+import java.util.List;
+
+@ApplicationScoped
+@Transactional
 public class CustomerRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<Customer> findAll() {
+        Query query = entityManager.createQuery("SELECT c FROM Customer c");
+        return (List<Customer>) query.getResultList();
+    }
 }
